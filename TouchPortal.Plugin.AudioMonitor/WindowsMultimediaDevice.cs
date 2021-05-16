d74cfa070d33f8710c6f5cb88ed677417ef07167
Oddbjørn Bakke <oddbjorn.bakke@gmail.com>
2021-05-16 04:03:34 +0200

    Recorder: DataAvailable test

--- TouchPortal.Plugin.AudioMonitor/WindowsMultimediaDevice.cs
+++ TouchPortal.Plugin.AudioMonitor/WindowsMultimediaDevice.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Threading;
 using NAudio.CoreAudioApi;
+using NAudio.Wave;
 
 namespace TouchPortal.Plugin.AudioMonitor
 {
     public class WindowsMultimediaDevice : IDisposable
     {
         private readonly IPluginCallbacks _callbacks;
-        
+
+        private const bool FEATURE_DATA_AVAILABLE = true;
+
         private MMDevice _mmDevice;
         private WasapiCapture _recorder;
         private Thread _monitoringThread;
@@ -48,6 +51,10 @@ namespace TouchPortal.Plugin.AudioMonitor
                 _mmDevice = devices[index];
 
                 _recorder = new WasapiCapture(_mmDevice);
+                if (FEATURE_DATA_AVAILABLE)
+                {
+                    _recorder.DataAvailable += DataAvailableEvent;
+                }
                 _recorder.StartRecording();
 
                 _callbacks.MultimediaDeviceUpdateCallback(_mmDevice.FriendlyName);
@@ -70,12 +77,15 @@ namespace TouchPortal.Plugin.AudioMonitor
         public void StartMonitoring()
         {
             ClearMonitoring();
-            
-            _monitoringThread = new Thread(Monitoring)
+
+            if (!FEATURE_DATA_AVAILABLE)
             {
-                IsBackground = true
-            };
-            _monitoringThread.Start();
+                _monitoringThread = new Thread(Monitoring)
+                {
+                    IsBackground = true
+                };
+                _monitoringThread.Start();
+            }
 
             IsMonitoring = true;
         }
@@ -93,6 +103,50 @@ namespace TouchPortal.Plugin.AudioMonitor
                 ClearMonitoring();
             else
                 StartMonitoring();
+        }
+
+        //TODO: Make a Windows tester (I think it's the latency between the Android device and this that are the biggest issue):
+        //From Docs: https://github.com/naudio/NAudio/blob/master/Docs/RecordingLevelMeter.md
+        //With this, WasapiCapture have to be changed for WasapiLoopbackCapture if Render mode (outputs):
+        private int _count = 0;
+        private double _temp = double.MinValue;
+
+        private void DataAvailableEvent(object obj, WaveInEventArgs args)
+        {
+            var max = 0f;
+            var buffer = new WaveBuffer(args.Buffer);
+
+            // interpret as 32 bit floating point audio (WasapiIn)
+            for (var index = 0; index < args.BytesRecorded / 4; index++)
+            {
+                //Ignore, this situation is ok.
+                var sample = buffer.FloatBuffer[index];
+
+                // absolute value 
+                if (sample < 0)
+                    sample = -sample;
+
+                // is this the max value?
+                if (sample > max)
+                    max = sample;
+            }
+
+            var decibel = Math.Log10(max) * 20;
+            decibel = Math.Round(decibel);
+
+            //It's to fast (easier to test this way than extending the buffers etc.):
+            if (_count < 2)
+            {
+                _count++;
+                if (decibel > _temp)
+                    _temp = decibel;
+            }
+            else
+            {
+                _count = 0;
+                _temp = double.MinValue;
+                _callbacks.MonitoringCallback(decibel);
+            }
         }
 
         private void Monitoring()
